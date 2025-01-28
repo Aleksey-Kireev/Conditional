36e7e72a8c300e1df3a6ac70f061112c16de7059
Aleksey-Kireev <kireev_a_a@mail.ru>
2025-01-29 00:39:15 +0900

    ----- Условные операторы ----- Первый коммит

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+//        ----- =====   Задание № 1   ===== -----
+
+        System.out.println();
+        System.out.println("----- =====   Задание № 1   ===== -----");
+        byte age = 21;
+        if (age >=18) {
+            System.out.println("Вам " + age + ", вы совершеннолетний.");
+        } else {
+            System.out.println("Вам " + age + ", вы не совершеннолетний." );
+        }
+
+
+
+
+
+
+    }
+}
