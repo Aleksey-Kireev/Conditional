e04c53d9495ecf94b21968d283422929cc28c6ce
Aleksey-Kireev <kireev_a_a@mail.ru>
2025-01-29 00:52:49 +0900

    ----- Условные операторы ----- промежуточный пуш. Создание ветки HW_1.

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,21 @@ public class Main {
             System.out.println("Вам " + age + ", вы не совершеннолетний." );
         }
 
+//        ----- =====   Задание № 2   ===== -----
+
+        System.out.println();
+        System.out.println("----- =====   Задание № 2   ===== -----");
+        byte temp = -14;
+        if (temp <=5) {
+            System.out.println("На улице " + temp + ", холодно, нужно надеть шапку.");
+        } else {
+            System.out.println("Сегодня +" + temp + ", тепло, можно идти без шапки.");
+        }
+
+//        ----- =====   Задание № 3   ===== -----
+
+        System.out.println();
+        System.out.println("----- =====   Задание № 3   ===== -----");
 
 
 
